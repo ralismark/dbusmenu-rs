daf54a758d1ef0ace0992db4747a86f0782b0bc0
ralismark <13449732+ralismark@users.noreply.github.com>
2023-04-04 15:27:07 +1000

    Package everything

--- flake.nix
+++ flake.nix
@@ -39,6 +39,7 @@
         mkGir = {
           pname,
           version,
+          workMode,
           options,
           fixup ? "",
           ...
@@ -56,6 +57,7 @@
             options = options // {
               library = pname;
               version = version;
+              work_mode = workMode;
               girs_directories = (options.girs_directories or []) ++ [
                 "${./gir}"
                 "${pkgs.libdbusmenu-gtk3}/share/gir-1.0"
@@ -69,21 +71,54 @@
             "fixup"
           ];
 
-          # ifMode = mode: s: if mode == work_mode then s else "";
+          ifMode = mode: s: if mode == workMode then s else "";
 
-          src = pkgs.runCommand "${pname}-${version}" attrs' ''
+          src = pkgs.runCommand "${pname}-${version}-${workMode}" attrs' ''
             mkdir $out
             ${pkgs.gir-rs}/bin/gir -c ${girToml} -o $out
 
             # fixup
-            rm $out/src/auto/versions.txt
+            shopt -s globstar
 
-            sed -i $out/src/lib.rs \
-              -e '/^#\[link/d' \
-              -e '/^\/\/from /d'
+            ${ifMode "sys" ''
+              sed -i $out/src/**/*.rs \
+                -e '/^#\[link/d'
 
-            sed -i $out/Cargo.toml \
-              -e 's/git = ".*"/version = "0.15"/'
+              sed -i $out/Cargo.toml \
+                -e 's/git = ".*"/version = "0.15"/'
+            ''}
+
+            ${ifMode "normal" ''
+              cat ${builtins.toFile "lib.rs" ''
+                /// No-op.
+                #[allow(unused_macros)]
+                macro_rules! skip_assert_initialized {
+                    () => {};
+                }
+
+                /// Asserts that this is the main thread and either `gdk::init` or `gtk::init` has been called.
+                #[allow(unused_macros)]
+                macro_rules! assert_initialized_main_thread {
+                    () => {
+                        if !::gtk::is_initialized_main_thread() {
+                            if ::gtk::is_initialized() {
+                                panic!("GTK may only be used from the main thread.");
+                            } else {
+                                panic!("GTK has not been initialized. Call `gtk::init` first.");
+                            }
+                        }
+                    };
+                }
+
+                mod auto;
+                pub use auto::*;
+              ''} > $out/src/lib.rs
+
+                sed -i $out/src/auto/mod.rs -e '/doc(hidden)/d'
+
+                mkdir -p $out/tests
+                echo "fn main() {}" > $out/tests/it_compiles.rs
+            ''}
 
             ${fixup}
           '';
@@ -92,32 +127,32 @@
       rec {
         packages.gir-rs = pkgs.gir-rs;
 
-        packages.libdbusmenu-sys = mkGir {
+        packages.dbusmenu-sys = mkGir {
           pname = "Dbusmenu";
           version = "0.4";
+          workMode = "sys";
 
           options = {
             min_cfg_version = "16";
-            work_mode = "sys";
             single_version_file = true;
             external_libraries = [
               "GLib"
               "GObject"
             ];
-            lib_version_overrides = [ {
-              version = "0.4";
-              lib_version = "0.17";
-            } ];
           };
+
+          fixup = ''
+            sed -e '/icon_paths/s/c_char/glib::GStrv/g' -i $out/src/lib.rs
+          '';
         };
 
-        packages.libdbusmenu-gtk3-sys = mkGir {
+        packages.dbusmenu-gtk3-sys = mkGir {
           pname = "DbusmenuGtk3";
           version = "0.4";
+          workMode = "sys";
 
           options = {
             min_cfg_version = "16";
-            work_mode = "sys";
             single_version_file = true;
             external_libraries = [
               "GLib"
@@ -130,15 +165,143 @@
 
           fixup = ''
             sed -i $out/Cargo.toml \
-              -e '/\[dependencies\]/adbusmenu = { package = "dbusmenu-sys", path = "../libdbusmenu-sys" }' \
+              -e '/\[dependencies\]/adbusmenu = { package = "dbusmenu-sys", path = "../dbusmenu-sys" }' \
               -e 's/^\(dox = \[\)\(.*\]\)$/\1"dbusmenu\/dox", \2/'
+          '';
+        };
+
+        packages.dbusmenu = mkGir {
+          pname = "Dbusmenu";
+          version = "0.4";
+          workMode = "normal";
+
+          options = {
+            min_cfg_version = "16";
+            single_version_file = true;
+            generate_safety_asserts = true;
+            deprecate_by_min_version = true;
+            generate = [
+              "Dbusmenu.Client"
+              "Dbusmenu.ClientTypeHandler"
+              "Dbusmenu.Menuitem"
+              "Dbusmenu.Status"
+              "Dbusmenu.TextDirection"
+              "Dbusmenu.menuitem_buildvariant_slot_t"
+              "Dbusmenu.menuitem_about_to_show_cb"
+              "Dbusmenu.MenuitemProxy"
+              "Dbusmenu.Server"
+            ];
+            manual = [
+              "GLib.DestroyNotify"
+              "GLib.Variant"
+            ];
+
+            object = [ {
+              name = "Dbusmenu.Client";
+              status = "generate";
+              function = [ {
+                # GStrv is not generated correctly
+                pattern = ".*";
+                ignore = true;
+              } ];
+            }  ];
+          };
+
+          fixup = ''
+            cat ${(pkgs.formats.toml {}).generate "Cargo.toml" {
+              package = {
+                name = "dbusmenu";
+                version = "0.0.1";
+                edition = "2021";
+
+                metadata.docs.rs.features = ["dox"];
+              };
+
+              lib.name = "dbusmenu";
+              dependencies = {
+                libc = "0.2";
+                ffi = { package = "dbusmenu-sys"; path = "../dbusmenu-sys"; };
+                glib = "0.15";
+                gtk = "0.15";
+              };
+
+              features.dox = ["ffi/dox" "glib/dox"];
+            }} > $out/Cargo.toml
+
+            sed -i $out/src/auto/menuitem.rs \
+              -e '/property_set_byte_array/s/value: u8, nelements: usize/values: \&[u8]/' \
+              -e '/ffi::dbusmenu_menuitem_property_set_byte_array/s/value, nelements/values.as_ptr(), values.len()/'
+          '';
+        };
+
+        packages.dbusmenu-gtk3 = mkGir {
+          pname = "DbusmenuGtk3";
+          version = "0.4";
+          workMode = "normal";
+
+          options = {
+            min_cfg_version = "16";
+            single_version_file = true;
+            generate_safety_asserts = true;
+            deprecate_by_min_version = true;
+            generate = [
+              "DbusmenuGtk3.Client"
+              "DbusmenuGtk3.Menu"
+            ];
+            manual = [
+              "Atk.ImplementorIface"
+              "Dbusmenu.Client"
+              "Dbusmenu.Menuitem"
+              "GObject.InitiallyUnowned"
+              "Gdk.ModifierType"
+              "GdkPixbuf.Pixbuf"
+              "Gtk.AccelGroup"
+              "Gtk.Buildable"
+              "Gtk.Container"
+              "Gtk.Menu"
+              "Gtk.MenuItem"
+              "Gtk.MenuShell"
+              "Gtk.Widget"
+            ];
+          };
+
+          fixup = ''
+            sed -i $out/src/**/menu.rs -e 's/gobject/glib::object/g'
+
+            # interface does not exist
+            sed -i $out/src/auto/menu.rs \
+              -e 's/atk::ImplementorIface, //'
+
+            cat >$out/Cargo.toml ${(pkgs.formats.toml {}).generate "Cargo.toml" {
+              package = {
+                name = "dbusmenu-gtk3";
+                version = "0.0.1";
+                edition = "2021";
+
+                metadata.docs.rs.features = ["dox"];
+              };
+
+              lib.name = "dbusmenu_gtk3";
+              dependencies = {
+                libc = "0.2";
+                ffi = { package = "dbusmenu-gtk3-sys"; path = "../dbusmenu-gtk3-sys"; };
+                dbusmenu = { path = "../dbusmenu"; };
+                glib = "0.15";
+                gtk = "0.15";
+                atk = "0.15";
+              };
+
+              features.dox = ["ffi/dox" "glib/dox"];
+            }}
           '';
         };
 
         packages.default = pkgs.runCommand "dbusmenu-crates" {} ''
           mkdir $out
-          ln -s ${packages.libdbusmenu-sys} $out/libdbusmenu-sys
-          ln -s ${packages.libdbusmenu-gtk3-sys} $out/libdbusmenu-gtk3-sys
+          ln -s ${packages.dbusmenu-sys} $out/dbusmenu-sys
+          ln -s ${packages.dbusmenu-gtk3-sys} $out/dbusmenu-gtk3-sys
+          ln -s ${packages.dbusmenu} $out/dbusmenu
+          ln -s ${packages.dbusmenu-gtk3} $out/dbusmenu-gtk3
         '';
 
         devShells.default = pkgs.mkShell {
